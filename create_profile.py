f13077baae6094156d7d8e0333849fc977b0532d
VaghesanSundaram <vaghesan@gmail.com>
2025-03-31 19:09:22 -0400

    takes first 5 inputs, compares to what it should be, refreshes

--- create_profile.py
+++ create_profile.py
@@ -3,30 +3,37 @@ from pynput import keyboard
 import threading
 
 text = []
-keyphrase = "a"
+keyphrase = "hello"
+
 def on_key_press(key): 
-    text.append(key.char)
-    print(key)
+    try:
+        text.append(key.char)
+        print(key)
+    except AttributeError:
+        text.append(str(key))
+    
     
 
 def on_key_release(key):
     print(text)
 
 def check():
-    if (len(text) == len(keyphrase)):
-        if ("".join(text) != keyphrase):
-            print("try again")
-            text = []
-            return
-        else:
-            print("done")
-            quit()
+    global text
+    while True:
+        if len(text) == len(keyphrase):
+            if text == ['h','e','l','l','o']:
+                print("Correct!")
+                text = []
+            else:
+                print("Nope")
+                text = []
 
 
 
 my_thread = threading.Thread(target=check)
 my_thread.start()
+
 with keyboard.Listener(on_press = on_key_press, on_release=on_key_release) as press_listener:
     press_listener.join()
-
-my_thread.join()
+    
+    my_thread.join()
